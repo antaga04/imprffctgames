8dff3e38f3cad6c043ea0398a90aaaee9cba1c8a
antaga04 <antaga04@gmail.com>
2024-12-29 13:31:46 +0100

    feat: add CoolDownButton component with cooldown functionality

---
+++ client/src/components/ui/CoolDownButton.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+type CoolDownButtonProps = {
+    text: string;
+    onSubmit: () => void;
+    bgColor?: string;
+    hoverBgColor?: string;
+    textColor?: string;
+    className?: string;
+};
+
+const CoolDownButton: React.FC<CoolDownButtonProps> = ({
+    onSubmit,
+    text,
+    bgColor = 'bg-blue-600',
+    hoverBgColor = 'hover:bg-blue-700',
+    textColor = 'text-white',
+    className,
+}) => {
+    const [cooldown, setCooldown] = useState<boolean>(false);
+
+    const handleOnSubmit = () => {
+        if (!cooldown) {
+            onSubmit();
+            setCooldown(true);
+            setTimeout(() => setCooldown(false), 1000);
+        }
+    };
+
+    return (
+        <button
+            onClick={handleOnSubmit}
+            disabled={cooldown}
+            className={`px-4 py-2 rounded ${
+                cooldown ? 'bg-gray-400 hover:bg-gray-500 cursor-not-allowed' : bgColor
+            } ${hoverBgColor} ${textColor} ${className ?? ''}`}
+        >
+            {text}
+        </button>
+    );
+};
+
+export default CoolDownButton;
