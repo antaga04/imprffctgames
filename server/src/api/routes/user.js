9bbe8ba366f19ed8024f30fa9871d531ede08497
antaga04 <antaga04@gmail.com>
2025-01-19 00:14:55 +0100

    feat: update server to use cookie token and add email confirmation
    add email confirmation functionality; update user routes; add controllers for http-only cookie; update login; make updateUserAccount only update nickname

--- server/src/api/routes/user.js
+++ server/src/api/routes/user.js
@@ -6,6 +6,9 @@ import {
     updateUserAvatar,
     getUser,
     updateUserAccount,
+    confirmEmail,
+    verifyUser,
+    logoutUser,
 } from '../controllers/user.js';
 import { hasValidAuthJwt } from '../../middlewares/authenticated.js';
 import uploadFile from '../../middlewares/uploadFile.js';
@@ -15,8 +18,13 @@ const router = express.Router();
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.put('/', hasValidAuthJwt, updateUserAccount);
+router.post('/confirm-email', confirmEmail);
+router.post('/resend-email', confirmEmail);
 router.put('/newpassword', hasValidAuthJwt, updateUserPassword);
 router.get('/', hasValidAuthJwt, getUser);
 router.put('/avatar', hasValidAuthJwt, uploadFile.single('avatar'), updateUserAvatar);
+
+router.get('/verify', hasValidAuthJwt, verifyUser);
+router.post('/logout', logoutUser);
 
 export default router;
---
+++ server/src/utils/email.js
@@ -0,0 +1,27 @@
+import { Resend } from 'resend';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Define __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const sendConfirmationEmail = async (email, token) => {
+    const resend = new Resend(process.env.RESEND_API_KEY);
+    // Send confirmation email
+    const emailTemplatePath = path.join(__dirname, '../../public/email.html');
+    const emailTemplate = fs.readFileSync(emailTemplatePath, 'utf-8');
+
+    const confirmationLink = `${process.env.CLIENT_URL}/confirm-email?token=${token}`;
+    const customizedEmail = emailTemplate.replace('{{confirmationLink}}', confirmationLink);
+
+    const { data, error } = await resend.emails.send({
+        from: 'PuzLynk <onboarding@resend.dev>',
+        to: [email],
+        subject: 'Email Confirmation',
+        html: customizedEmail,
+    });
+
+    return { data, error };
+};
