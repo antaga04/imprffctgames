851d6dd288e4d865de6827275dd9602bbe2f5d97
antaga04 <antaga04@gmail.com>
2025-01-26 15:42:16 +0100

    fix: add parser to make page and limit numbers

--- server/src/utils/paginationHelper.js
+++ server/src/utils/paginationHelper.js
@@ -6,15 +6,18 @@
  * @returns {Object} - Paginated data and pagination metadata.
  */
 export const paginate = (data, page, limit) => {
-    const startIndex = (page - 1) * limit;
-    const endIndex = startIndex + limit;
+    const pageNumber = parseInt(page, 10) || 1;
+    const limitNumber = parseInt(limit, 10) || 10;
+
+    const startIndex = (pageNumber - 1) * limitNumber;
+    const endIndex = startIndex + limitNumber;
 
     const paginatedData = data.slice(startIndex, endIndex);
 
     const pagination = {
         totalItems: data.length,
-        currentPage: Number(page),
-        totalPages: Math.ceil(data.length / limit),
+        currentPage: pageNumber,
+        totalPages: Math.ceil(data.length / limitNumber),
         hasNextPage: endIndex < data.length,
         hasPreviousPage: startIndex > 0,
     };
