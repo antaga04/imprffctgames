589aa0a71bde333d0aabb7f58bc1c933bb9b0bdd
antaga04 <antaga04@gmail.com>
2025-02-08 17:07:32 +0100

    feat: add email validation middleware and MX record lookup

--- server/src/api/routes/user.js
+++ server/src/api/routes/user.js
@@ -13,16 +13,24 @@ import {
 } from '../controllers/user.js';
 import { hasValidAuthJwt } from '../../middlewares/authenticated.js';
 import uploadFile from '../../middlewares/uploadFile.js';
-import { resendConfirmationLimiter, avatarLimiter } from '../../utils/rateLimiters.js';
+import { resendConfirmationLimiter, avatarLimiter, registerLimiter } from '../../middlewares/rateLimiters.js';
+import { emailValidationMiddleware } from '../../middlewares/emailValidationAPI.js';
+import { mxRecordLookup } from '../../middlewares/mxRecordLookup.js';
 
 const router = express.Router();
 
-router.post('/register', registerUser);
+router.post('/register', registerLimiter, mxRecordLookup, emailValidationMiddleware, registerUser);
 router.post('/login', loginUser);
 router.put('/', hasValidAuthJwt, updateUserAccount);
 
-router.post('/confirm-email', confirmEmail);
-router.post('/resend-email', resendConfirmationLimiter, resendConfirmationEmail);
+router.post('/confirm-email', mxRecordLookup, emailValidationMiddleware, confirmEmail);
+router.post(
+    '/resend-email',
+    resendConfirmationLimiter,
+    mxRecordLookup,
+    emailValidationMiddleware,
+    resendConfirmationEmail,
+);
 
 router.put('/newpassword', hasValidAuthJwt, updateUserPassword);
 router.get('/', hasValidAuthJwt, getUser);
