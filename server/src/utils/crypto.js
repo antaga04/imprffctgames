b157d58ffc0a4c19ba549303fdebc016bdc243f4
antaga04 <antaga04@gmail.com>
2025-02-10 22:00:15 +0100

    feat: add generateBoardHash function and refactor decryptData to use function declaration

--- server/src/utils/crypto.js
+++ server/src/utils/crypto.js
@@ -1,6 +1,7 @@
 import CryptoJS from 'crypto-js';
+import { createHmac } from 'crypto';
 
-export const decryptData = (ciphertext) => {
+export function decryptData(ciphertext) {
     try {
         const bytes = CryptoJS.AES.decrypt(ciphertext, process.env.CRYPTO_SECRET_KEY);
         const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
@@ -9,4 +10,8 @@ export const decryptData = (ciphertext) => {
         console.error('[decryptData] Error:', error);
         throw new Error('Invalid or corrupted data');
     }
-};
+}
+
+export function generateBoardHash(data) {
+    return createHmac('sha256', process.env.HASH_SECRET_KEY).update(JSON.stringify(data)).digest('hex');
+}
