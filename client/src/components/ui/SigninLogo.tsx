e31890206f86e4abbce2d3d6206eda11e6fc8562
antaga04 <antaga04@gmail.com>
2025-07-01 20:22:03 +0200

    feat: change logo in every part of the app

--- client/src/components/ui/SigninLogo.tsx
+++ client/src/components/ui/SigninLogo.tsx
@@ -1,7 +1,7 @@
 const SigninLogo: React.FC = () => {
     return (
         <div className="bg-[#6795df] text-white p-4 md:pt-20 rounded-md flex gap-4 items-center">
-            <img src="/delta.png" alt="Logo Imprffct Games" draggable="false" className="w-16 h-auto" />
+            <img src="/imprffct.svg" alt="Logo Imprffct Games" draggable="false" className="w-16 h-auto" />
             <div className=" flex flex-col p-2 tems-start justify-start">
                 <h1 className="text-3xl">
                     <span className="error-underline neon-text">imprffct</span> games
