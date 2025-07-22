76d230bf7b4390a72e82de36e9ee9366426a91cd
antaga04 <antaga04@gmail.com>
2025-07-22 23:47:41 +0200

    test to fix alias imports in vercel production

---
+++ server/tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+    entry: ['src/index.ts'],
+    format: ['esm'],
+    outDir: 'dist',
+    sourcemap: true,
+    clean: true,
+    dts: true,
+    esbuildOptions(options) {
+        options.alias = {
+            '@': './src',
+        };
+    },
+});
