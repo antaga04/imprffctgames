13e6942767460be5e09a25768f5c9d890e9e8d6c
antaga04 <antaga04@gmail.com>
2025-07-23 17:28:06 +0200

    wip

---
+++ server/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+    entry: ['src/index.ts'],
+    format: ['esm'],
+    outDir: 'dist',
+    clean: true,
+    dts: true,
+    minify: true,
+    sourcemap: true,
+    splitting: false,
+    tsconfig: './tsconfig.json',
+});
