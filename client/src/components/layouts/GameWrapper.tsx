7ea3da39658b2bed5fa3acf7252b882a012cd8e2
antaga04 <antaga04@gmail.com>
2025-07-31 15:02:18 +0200

    feat: add info icon to instructions

--- client/src/components/layouts/GameWrapper.tsx
+++ client/src/components/layouts/GameWrapper.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import BackButton from '@/components/ui/BackButton';
+import { Info } from 'lucide-react';
 
 const GameWrapper: React.FC<GameWrapperProps> = ({ children, title, height, instructions }) => {
     return (
@@ -8,7 +9,9 @@ const GameWrapper: React.FC<GameWrapperProps> = ({ children, title, height, inst
             <div className={`flex flex-col justify-start items-center min-h-[${height}] md:mt-40 mt-24`}>
                 <h1 className="text-4xl font-bold text-center neon-text text-white mb-4">{title}</h1>
                 {children}
-                <p className="mt-4 text-center">{instructions}</p>
+                <p className="flex gap-2 mt-4 text-center">
+                    <Info /> {instructions}
+                </p>
             </div>
         </>
     );
