6cdcfa36c96835a2423793520759878541d0a6f5
antaga04 <antaga04@gmail.com>
2025-08-15 14:50:56 +0200

    feat: enhance password validation with detailed error reporting; add function to validate requiered fields

--- server/src/utils/validation.ts
+++ server/src/utils/validation.ts
@@ -1,3 +1,5 @@
+import { PasswordValidationErrorKey, PasswordValidationResult } from '@/types';
+
 const emailRegex =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
@@ -15,25 +17,49 @@ export const validateEmail = (email: string) => {
     return { valid: true };
 };
 
-export const validatePassword = (password: string) => {
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/;
-    if (password == null || !password || typeof password !== 'string' || !passwordRegex.test(password)) {
+export const validatePassword = (password: string): PasswordValidationResult => {
+    const errors: Partial<Record<PasswordValidationErrorKey, string>> = {};
+
+    if (typeof password !== 'string' || password.trim() === '') {
         return {
             valid: false,
-            message:
-                'Password must be at least 6 characters long, contain at least one uppercase letter, one lowercase, one digit and one special character.',
+            errors: {
+                general: 'password.invalid_type_or_empty',
+            },
         };
     }
-    return { valid: true };
+
+    if (password.length < 6) {
+        errors.length = 'password.too_short';
+    }
+
+    if (!/[a-z]/.test(password)) {
+        errors.lowercase = 'password.missing_lowercase';
+    }
+
+    if (!/[A-Z]/.test(password)) {
+        errors.uppercase = 'password.missing_uppercase';
+    }
+
+    if (!/\d/.test(password)) {
+        errors.digit = 'password.missing_digit';
+    }
+
+    if (!/[\W_]/.test(password)) {
+        errors.specialChar = 'password.missing_special_char';
+    }
+
+    return Object.keys(errors).length > 0
+        ? { valid: false, errors: errors as Record<PasswordValidationErrorKey, string> }
+        : { valid: true };
 };
 
-export const runValidations = (
-    fields: [string, ReturnType<typeof validateEmail | typeof validatePassword | typeof validateNickname>][],
-) => {
-    for (const [field, result] of fields) {
-        if (!result.valid) {
-            return { valid: false, message: `${field}: ${result.message}` };
+export function validateRequiredFields(body: Record<string, any>, fields: string[]): Record<string, string> {
+    const errors: Record<string, string> = {};
+    fields.forEach((field) => {
+        if (!body[field]) {
+            errors[field] = `${field} is required`;
         }
-    }
-    return { valid: true };
-};
+    });
+    return errors;
+}
