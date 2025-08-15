1ecf6686ea16076654dbe0290a30d5071c4c230d
antaga04 <antaga04@gmail.com>
2025-08-15 14:56:40 +0200

    refactor: remove unused functions

--- client/src/lib/encrypt.ts
+++
@@ -1,6 +0,0 @@
-import CryptoJS from 'crypto-js';
-
-export const encryptData = (data: unknown) => {
-    const ciphertext = CryptoJS.AES.encrypt(JSON.stringify(data), import.meta.env.VITE_CRYPTO_SECRET_KEY).toString();
-    return ciphertext;
-};
