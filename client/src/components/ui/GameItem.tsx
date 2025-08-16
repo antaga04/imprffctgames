23910ad6a2b715797ddb513b7e2250fa507b9ae2
antaga04 <antaga04@gmail.com>
2025-08-16 20:15:58 +0200

    feat: add loading lazy and change thumbnails to cloudinary ones

--- client/src/components/ui/GameItem.tsx
+++ client/src/components/ui/GameItem.tsx
@@ -10,7 +10,7 @@ const GameItem: React.FC<GameItemProps> = ({ link, name, thumbnail }) => {
                 className="card p-4 justify-start w-full h-full flex gap-4 sm:justify-center items-center md:flex-col bg-[linear-gradient(190deg,#cddbe2b5,#7886949c)] border border-[#d0d0d059] hover:border-[#d3d3d3] rounded-[16px] no-underline transition-[border] duration-300 ease-custom-ease-1 relative overflow-hidden"
             >
                 <div className="glare-item-top inner-edge"></div>
-                <img src={thumbnail} alt={`${name} logo`} className="w-10 h-10 md:w-16 md:h-16 z-10" />
+                <img loading="lazy" src={thumbnail} alt={`${name} logo`} className="w-10 h-10 md:w-16 md:h-16 z-10" />
                 <p className="text-2xl font-bold text-white z-10 text-center">{name}</p>
                 <div className="card-bg"></div>
             </Link>
--- client/src/lib/games.ts
+++ client/src/lib/games.ts
@@ -2,25 +2,25 @@ export const games = {
     tictactoe: {
         name: 'Tic Tac Toe',
         url: 'tictactoe',
-        thumbnail: '/tictactoe.png',
+        thumbnail: 'https://res.cloudinary.com/drsfru9lj/image/upload/v1755299859/imprffctgames/games/tictactoe.webp',
         loader: () => import('@/games/TicTacToe').then((m) => m.default),
     },
     puzzle15: {
         name: '15 Puzzle',
         url: 'puzzle15',
-        thumbnail: '/15puzzle.png',
+        thumbnail: 'https://res.cloudinary.com/drsfru9lj/image/upload/v1755299856/imprffctgames/games/15puzzle.webp',
         loader: () => import('@/games/Puzzle15').then((m) => m.default),
     },
     pokemon: {
         name: "Who's that Pokémon",
         url: 'pokemon',
-        thumbnail: '/pokemon.png',
+        thumbnail: 'https://res.cloudinary.com/drsfru9lj/image/upload/v1755299858/imprffctgames/games/pokemon.webp',
         loader: () => import('@/games/Pokemon').then((m) => m.default),
     },
     lizardtype: {
         name: 'Lizardtype',
         url: 'lizardtype',
-        thumbnail: '/lizardtype.png',
+        thumbnail: 'https://res.cloudinary.com/drsfru9lj/image/upload/v1755299857/imprffctgames/games/lizardtype.webp',
         loader: () => import('@/games/LizardType').then((m) => m.default),
     },
 };
