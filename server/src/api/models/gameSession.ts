59f34daa24a3f10eb82e4c16436ad9bf231f6c99
antaga04 <antaga04@gmail.com>
2025-08-20 13:38:55 +0200

    feat: add to game Sessicon the fields user_id, gameplay and change game_id for game_slug; change expiracy to 24h

--- server/src/api/models/gameSession.ts
+++ server/src/api/models/gameSession.ts
@@ -3,10 +3,14 @@ import mongoose from 'mongoose';
 
 const gameSessionSchema = new mongoose.Schema<GameSessionSchema>(
     {
-        game_id: {
-            type: mongoose.Schema.Types.ObjectId,
+        game_slug: {
+            type: String,
             required: true,
             ref: 'Game',
+        },
+        user_id: {
+            type: String, // TODO: change it to object id and be required
+            ref: 'User',
         },
         hash: {
             type: String,
@@ -18,10 +22,11 @@ const gameSessionSchema = new mongoose.Schema<GameSessionSchema>(
         validatedResults: { type: Object, default: null },
         session_expiry: {
             type: Date,
-            default: () => new Date(Date.now() + 60 * 60 * 1000), // 1 hour
+            default: () => new Date(Date.now() + 24 * 60 * 60 * 1000), // 24 hours
             index: { expires: 0 }, // ⬅ This makes MongoDB automatically delete expired docs
         },
         variant: { type: String, required: false },
+        gameplay: { type: Object, default: null },
     },
     {
         timestamps: true,
