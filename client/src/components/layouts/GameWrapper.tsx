bd34e61c738c06d403f5e79dcb70c387a38ea5b4
antaga04 <antaga04@gmail.com>
2025-08-21 19:23:49 +0200

    style: add loading fallback to instructions in GameWrapper and add bottom margin

--- client/src/components/layouts/GameWrapper.tsx
+++ client/src/components/layouts/GameWrapper.tsx
@@ -9,8 +9,8 @@ const GameWrapper: React.FC<GameWrapperProps> = ({ children, title, height, inst
             <div className={`flex flex-col justify-start items-center min-h-[${height}] md:mt-40 mt-24`}>
                 <h1 className="text-4xl font-bold text-center neon-text text-white mb-4">{title}</h1>
                 {children}
-                <p className="flex gap-2 mt-4 text-center">
-                    <Info /> {instructions}
+                <p className="flex gap-2 mt-4 mb-8 text-center">
+                    <Info /> {instructions ?? 'Loading instructions...'}
                 </p>
             </div>
         </>
