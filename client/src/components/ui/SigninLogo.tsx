7b3561b8e4bd1265e29c4f72d99e2b9a121a0e57
antaga04 <antaga04@gmail.com>
2025-08-29 15:15:47 +0200

    style: fix class typo in SinginLogo

--- client/src/components/ui/SigninLogo.tsx
+++ client/src/components/ui/SigninLogo.tsx
@@ -2,7 +2,7 @@ const SigninLogo: React.FC = () => {
     return (
         <div className="bg-[#6795df] text-white p-4 md:pt-20 rounded-md flex gap-4 items-center">
             <img src="/imprffct.svg" alt="Logo Imprffct Games" draggable="false" className="w-16 h-auto" />
-            <div className=" flex flex-col p-2 tems-start justify-start">
+            <div className=" flex flex-col p-2 items-start justify-start">
                 <h1 className="text-3xl">
                     <span className="error-underline neon-text">imprffct</span> games
                 </h1>
