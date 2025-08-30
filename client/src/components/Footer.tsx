c27d20ce89dcfa4b34934a149836eebb3d84d7bd
antaga04 <antaga04@gmail.com>
2025-08-31 00:11:29 +0200

    feat: add link to terms page and improve styles of the links layout

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
     return (
         <footer className="md:w-full flex items-center justify-between text-[#f2f2f2] py-4 md:py-6 border-t border-[#f2f2f21a]">
             <div className="w-full flex flex-col md:flex-row-reverse md:justify-between md:items-center">
-                <div className="flex md:gap-10 gap-4 flex-wrap">
+                <div className="flex gap-4 flex-wrap w-full md:justify-end">
                     <div className="">
                         <p className="text-xs opacity-50 tracking-wider mb-2 uppercase">{t('footer.main')}</p>
                         <div className="flex gap-2">
@@ -20,6 +20,12 @@ const Footer = () => {
                                 className="capitalize w-full flex items-center justify-between gap-2 group md:px-3 md:py-1 rounded-lg md:bg-[#f2f2f20f] md:hover:bg-[#f2f2f233] md:border md:border-[#f2f2f20a] md:hover:border-[#f2f2f21a] md:backdrop-blur-md md:transition-all md:duration-400 ease-custom-ease-1"
                             >
                                 {t('globals.ranking')}
+                            </Link>
+                            <Link
+                                to="/terms"
+                                className="capitalize w-full flex items-center justify-between gap-2 group md:px-3 md:py-1 rounded-lg md:bg-[#f2f2f20f] md:hover:bg-[#f2f2f233] md:border md:border-[#f2f2f20a] md:hover:border-[#f2f2f21a] md:backdrop-blur-md md:transition-all md:duration-400 ease-custom-ease-1"
+                            >
+                                {t('globals.terms')}
                             </Link>
                             {isAuthenticated ? (
                                 <Link
@@ -31,7 +37,7 @@ const Footer = () => {
                             ) : (
                                 <Link
                                     to="/login"
-                                    className="capitalize w-full flex items-center justify-between gap-2 group md:px-3 md:py-1 rounded-lg md:bg-[#f2f2f20f] md:hover:bg-[#f2f2f233] md:border md:border-[#f2f2f20a] md:hover:border-[#f2f2f21a] md:backdrop-blur-md md:transition-all md:duration-400 ease-custom-ease-1"
+                                    className="text-nowrap capitalize w-full flex items-center justify-between gap-2 group md:px-3 md:py-1 rounded-lg md:bg-[#f2f2f20f] md:hover:bg-[#f2f2f233] md:border md:border-[#f2f2f20a] md:hover:border-[#f2f2f21a] md:backdrop-blur-md md:transition-all md:duration-400 ease-custom-ease-1"
                                 >
                                     {t('globals.login')}
                                 </Link>
@@ -43,12 +49,13 @@ const Footer = () => {
                         <div className="flex gap-2">
                             <SocialLink name="LinkedIn" link="https://www.linkedin.com/in/adrian-anta-gil/" />
                             <SocialLink name="GitHub" link="https://github.com/antaga04" />
+                            <SocialLink name="Email" link="mailto:antaga04@gmail.com" />
                         </div>
                     </div>
                 </div>
-                <div className="logo flex items-center gap-4">
+                <div className="logo flex items-center gap-4 w-full mt-4 md:m-0">
                     <div>
-                        <p className="mt-4 opacity-65 md:opacity-90 md:m-0">
+                        <p className="opacity-65 md:opacity-90">
                             {t('footer.author')}{' '}
                             <a
                                 href="https://adrian-anta.netlify.app/"
@@ -61,7 +68,7 @@ const Footer = () => {
                         </p>
                         <p className="opacity-65 text-sm hidden md:block">{t('footer.madewith')}</p>
                     </div>
-                    <div className="w-fit md:ml-5">
+                    <div className="w-fit">
                         <LanguageSelector />
                     </div>
                 </div>
