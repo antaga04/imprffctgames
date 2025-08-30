d7c7720c99fd92ddc2c94609bb3a0e5b8db25f5d
antaga04 <antaga04@gmail.com>
2025-08-30 21:11:28 +0200

    refactor: change i18n code in mongo error handler in server utils/error.ts

--- server/src/utils/error.ts
+++ server/src/utils/error.ts
@@ -5,7 +5,7 @@ import { sendError } from './response';
 export const handleMongooseError = (err: any, res: Response) => {
     if (err instanceof mongoose.Error.ValidationError) {
         return sendError(res, 400, {
-            i18n: 'server.error.validation',
+            i18n: 'mongo.error.validation',
             message: 'Validation error',
             errors: err.errors,
         });
@@ -14,14 +14,14 @@ export const handleMongooseError = (err: any, res: Response) => {
     if (err.code === 11000) {
         const field = Object.keys(err.keyValue)[0];
         return sendError(res, 400, {
-            i18n: 'server.error.duplicate',
+            i18n: 'mongo.error.duplicate',
             message: `${field} is already in use.`,
         });
     }
 
     console.error('Unexpected error:', err);
     return sendError(res, 500, {
-        i18n: 'server.error.unexpected',
+        i18n: 'mongo.error.unexpected',
         message: 'Unexpected error occurred',
     });
 };
