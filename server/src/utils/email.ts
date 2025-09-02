1373ccc8d79e4521d90576d8f32996521115fef3
antaga04 <antaga04@gmail.com>
2025-09-02 19:03:04 +0200

    feat: add function to send email for account deletion and fix path join to resolve for vercel

--- server/src/utils/email.ts
+++ server/src/utils/email.ts
@@ -13,7 +13,7 @@ export const sendConfirmationEmail = async (
 ): Promise<{ data: CreateEmailResponseSuccess | null; error: ErrorResponse | null }> => {
     const resend = new Resend(process.env.RESEND_API_KEY);
     // Load and customize email template
-    const emailTemplatePath = path.join(__dirname, '../../public/email.html');
+    const emailTemplatePath = path.resolve(__dirname, '../../public/email.html');
     const emailTemplate = fs.readFileSync(emailTemplatePath, 'utf-8');
 
     const confirmationLink = `${process.env.CLIENT_URL}/confirm-email?token=${token}`;
@@ -37,7 +37,7 @@ export const sendResetPasswordEmail = async (
     const resend = new Resend(process.env.RESEND_API_KEY);
 
     // Load and customize email template
-    const emailTemplatePath = path.join(__dirname, '../../public/resetPassword.html');
+    const emailTemplatePath = path.resolve(__dirname, '../../public/resetPassword.html');
     const emailTemplate = fs.readFileSync(emailTemplatePath, 'utf-8');
 
     const resetPasswordLink = `${process.env.CLIENT_URL}/reset-password?token=${encodeURIComponent(token)}`;
@@ -48,6 +48,30 @@ export const sendResetPasswordEmail = async (
         from: 'Imprffct Games <noreply@auth.imprffctgames.com>',
         to: [email],
         subject: 'Password Reset',
+        html: customizedEmail,
+    });
+
+    return { data, error };
+};
+
+export const sendAccountDeletionEmail = async (
+    email: string,
+    token: string,
+): Promise<{ data: CreateEmailResponseSuccess | null; error: ErrorResponse | null }> => {
+    const resend = new Resend(process.env.RESEND_API_KEY);
+
+    // Load and customize email template
+    const emailTemplatePath = path.resolve(__dirname, '../../public/accountDeletion.html');
+    const emailTemplate = fs.readFileSync(emailTemplatePath, 'utf-8');
+
+    const accountDeletionLink = `${process.env.CLIENT_URL}/delete-account?token=${encodeURIComponent(token)}`;
+    const customizedEmail = emailTemplate.replace('{{accountDeletionLink}}', accountDeletionLink);
+
+    // Send account deletion email
+    const { data, error } = await resend.emails.send({
+        from: 'Imprffct Games <noreply@auth.imprffctgames.com>',
+        to: [email],
+        subject: 'Account Deletion',
         html: customizedEmail,
     });
 
