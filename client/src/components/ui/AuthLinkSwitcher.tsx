619e56c7c0f715aff908ed58f7afd81cfcecbb7c
antaga04 <antaga04@gmail.com>
2025-09-02 19:14:21 +0200

    feat: improve AuthLinkSwitcher texts and add link to resend confirmation email

--- client/src/components/ui/AuthLinkSwitcher.tsx
+++ client/src/components/ui/AuthLinkSwitcher.tsx
@@ -1,14 +1,22 @@
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
-const AuthLinkSwitcher: React.FC<AuthLinkSwitcherTypes> = ({ text, url, anchor }) => {
+const AuthLinkSwitcher: React.FC<AuthLinkSwitcherTypes> = ({ url, anchor }) => {
+    const { t } = useTranslation();
     return (
-        <div className="text-center mt-4">
-            <span>{text}</span>
+        <div className="mt-4 flex gap-2 flex-wrap items-center justify-center">
             <Link
                 to={url}
-                className="text-[#4b6a9d] hover:text-[#35517c] hover:underline ml-2 transition-colors ease-in-out duration-200"
+                className="text-[#4b6a9d] hover:text-[#35517c] hover:underline transition-colors ease-in-out duration-200"
             >
                 {anchor}
+            </Link>
+            <span className="hidden md:inline">|</span>
+            <Link
+                to="/resend-email"
+                className="text-[#4b6a9d] hover:text-[#35517c] hover:underline transition-colors ease-in-out duration-200"
+            >
+                {t('auth.resend_email')}
             </Link>
         </div>
     );
