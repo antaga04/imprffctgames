c89d5e3d5a908eeec0089053c9f4388471b7978d
antaga04 <antaga04@gmail.com>
2025-09-04 05:25:50 +0200

    server.feat: update lizardtype handlers to use new functions

--- server/src/handlers/lizardtype.ts
+++ server/src/handlers/lizardtype.ts
@@ -1,5 +1,8 @@
+import { validateLizardtypeScoreData } from '@/api/validations/lizardtype.validation';
+import { compareLizardtypeScores, sortLizardtypeScores } from '@/utils/scoreUtils';
+
 export const lizardtypeHandlers = {
-    validate: () => console.log('validate lizardtype'),
-    compare: () => console.log('compare lizardtype'),
-    sortScores: (scores: unknown) => scores,
+    validate: validateLizardtypeScoreData,
+    compare: compareLizardtypeScores,
+    sortScores: sortLizardtypeScores,
 };
