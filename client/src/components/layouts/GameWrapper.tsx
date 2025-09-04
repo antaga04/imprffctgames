4ef5b750d9e667955662e9fe21ef9598c5f33c03
antaga04 <antaga04@gmail.com>
2025-09-04 05:13:10 +0200

    style: center instructions icon [GameWrapper]

--- client/src/components/layouts/GameWrapper.tsx
+++ client/src/components/layouts/GameWrapper.tsx
@@ -11,7 +11,7 @@ const GameWrapper: React.FC<GameWrapperProps> = ({ children, title, height, inst
             <div className={`flex flex-col justify-start items-center min-h-[${height}] md:mt-40 mt-24`}>
                 <h1 className="text-4xl font-bold text-center neon-text text-white mb-4">{title}</h1>
                 {children}
-                <p className="flex gap-2 mt-4 mb-8 text-center">
+                <p className="flex gap-2 mt-4 mb-8 text-center flex-wrap justify-center">
                     <Info /> {instructions ?? t('games.loading_instructions') + '...'}
                 </p>
             </div>
