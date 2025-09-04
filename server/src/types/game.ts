c614dfe4df3d8f85c11119e4a98aeb743774d05b
antaga04 <antaga04@gmail.com>
2025-09-04 05:22:32 +0200

    server.types: addtypes for lizardtype

--- server/src/types/game.ts
+++ server/src/types/game.ts
@@ -35,3 +35,42 @@ type StoredPokemonScore = {
         },
     ];
 };
+type LizardtypeNewScore = {
+    wpm: number;
+    accuracy: number;
+};
+
+interface TypingMetrics {
+    wpm: number; // total number of characters in the correctly typed words (not including spaces)
+    raw: number; // like wpm but with all keystrokes except special keys
+    accuracy: number; // percentage of correct keystrokes
+    consistency: number; // spacing between keystrokes
+    correct: number; // correct characters of the end result
+    incorrect: number; // incorrect characters of the end result
+    hits: number; // correct characters that were typed
+    mistakes: number; // mistakes made during typing
+    missed: number; // mistakes made during typing
+}
+
+/* front */
+interface LizardtypeStats {
+    wpm: number;
+    accuracy: number;
+    correctChars: number;
+    totalChars: number;
+    totalMistakes: number;
+}
+
+interface LizardtypeScore extends LizardtypeStats {
+    hash: string;
+    gameSessionId: string | null;
+    keystrokes: KeystrokeData[];
+    variant: string;
+}
+
+type KeystrokeData = {
+    key: string;
+    timestamp: number;
+    duration?: number;
+    spacing?: number;
+};
